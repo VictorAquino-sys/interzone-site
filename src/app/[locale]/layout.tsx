a8f96766a45f03a7831a07712a4c289e0a0753e4
Victor A <aquinov@oregonstate.edu>
2025-07-09 11:20:10 -0700

    fix: use correct types for LocaleLayout to satisfy ESLint

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,24 +2,27 @@ import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import Navbar from '../components/Navbar';
 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
+// The new correct props type for async layouts in Next.js 15+
+type Props = {
   children: React.ReactNode;
-  params: { locale: string };
-}) {
+  params: { locale: string } | Promise<{ locale: string }>;
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+  // Support params as promise OR object:
+  const { locale } = await Promise.resolve(params);
+
   let messages;
   try {
-    messages = (await import(`../../messages/${params.locale}.json`)).default;
+    messages = (await import(`../../messages/${locale}.json`)).default;
   } catch {
     notFound();
   }
 
   return (
-    <html lang={params.locale}>
+    <html lang={locale}>
       <body>
-        <NextIntlClientProvider locale={params.locale} messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <Navbar />
           {children}
         </NextIntlClientProvider>
