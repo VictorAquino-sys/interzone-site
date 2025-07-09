874f82f77190559990a68d9a3381c32fec303e85
Victor A <aquinov@oregonstate.edu>
2025-07-09 13:51:10 -0700

    Add i18n setup and prepare for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 // src/app/layout.tsx
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">{children}</main>
+      </body>
+    </html>
+  );
+}
